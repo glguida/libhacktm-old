cd078877e3100951a64613901023de3b6dc1537c
Gianluca Guida <glguida@gmail.com>
2010-11-21 23:43:40 +0100

    Fix compilation in non-64 bit systems

--- Makefile
+++ Makefile
@@ -12,15 +12,15 @@ clean: tests_clean
 
 tests: test0 test1 test3
 test0: ${LIBNAME} tests/test0.c
-	$(CC) $(CFLAGS) -I. -L. -lhacktm tests/$@.c -o $@
+	$(CC) $(CFLAGS) -I. -L. tests/$@.c -lhacktm -o $@
 test1: ${LIBNAME} tests/test1.c
-	$(CC) $(CFLAGS) -I. -L. -lhacktm -lncurses tests/$@.c -o $@
+	$(CC) $(CFLAGS) -I. -L. tests/$@.c -lhacktm -lncurses -o $@
 test3: ${LIBNAME} tests/test3.c
-	$(CC) $(CFLAGS) -I. -L. -lhacktm tests/$@.c -o $@
+	$(CC) $(CFLAGS) -I. -L. tests/$@.c -lhacktm -o $@
 
 # This test requires PortAudio and single-precision FFTW3 libraries.
 test_audio: ${LIBNAME} tests/test_audio.c
-	$(CC) $(CFLAGS) -I . -L. -lhacktm -lportaudio -lm -lfftw3f -lncurses tests/$@.c -o $@
+	$(CC) $(CFLAGS) -I . -L. tests/$@.c -lhacktm -lportaudio -lm -lfftw3f -lncurses -o $@
 
 tests_clean:
 	-rm test0 test1 test3 test_audio
--- fdr.c
+++ fdr.c
@@ -27,18 +27,18 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <strings.h>
+#include <string.h>
 #include "fdr.h"
 
 size_t
 word_hweight(uint64_t x)
 {
-  x = ( x & 0x5555555555555555 ) + ( (x>>1) & 0x5555555555555555 );  
-  x = ( x & 0x3333333333333333 ) + ( (x>>2) & 0x3333333333333333 );  
-  x = ( x & 0x0F0F0F0F0F0F0F0F ) + ( (x>>4) & 0x0F0F0F0F0F0F0F0F );  
-  x = ( x & 0x00FF00FF00FF00FF ) + ( (x>>8) & 0x00FF00FF00FF00FF );  
-  x = ( x & 0x0000FFFF0000FFFF ) + ( (x>>16) & 0x0000FFFF0000FFFF );  
-  x = ( x & 0x00000000FFFFFFFF ) + ( (x>>32) & 0x00000000FFFFFFFF );  
+  x = ( x & 0x5555555555555555LL ) + ( (x>>1) & 0x5555555555555555LL );  
+  x = ( x & 0x3333333333333333LL ) + ( (x>>2) & 0x3333333333333333LL );  
+  x = ( x & 0x0F0F0F0F0F0F0F0FLL ) + ( (x>>4) & 0x0F0F0F0F0F0F0F0FLL );  
+  x = ( x & 0x00FF00FF00FF00FFLL ) + ( (x>>8) & 0x00FF00FF00FF00FFLL );  
+  x = ( x & 0x0000FFFF0000FFFFLL ) + ( (x>>16) & 0x0000FFFF0000FFFFLL );  
+  x = ( x & 0x00000000FFFFFFFFLL ) + ( (x>>32) & 0x00000000FFFFFFFFLL );  
   return (size_t)x;  
 }
 
